6177869856ad11bdfc8f8735304155a52145b777
Laura Dacol <laura.dacol@gmail.com>
2019-04-02 17:32:48 -0300

    death function

--- kot.js
+++ kot.js
@@ -64,6 +64,25 @@ class player{
 		this.money = money;
 		this.tokyo = tokyo;
 	}
+	
+	isDead(){
+		if(this.life <=9){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	colorsOfDeath(){
+		document.getElementById(this.id).style.filter = "grayscale(100%)";
+		document.getElementById(this.id+"Life").style.background = "black";
+		document.getElementById(this.id+"Points").style.background = "black";
+		document.getElementById(this.id+"Money").style.background = "black";			
+		document.getElementById(this.id+"Life").style.color = "red";
+		document.getElementById(this.id+"Points").style.color = "grey";
+		document.getElementById(this.id+"Money").style.color = "grey";	
+	}
 }
 
 class roundResults{
@@ -137,12 +156,22 @@ class gameRound{
 		this.countRound = 0;
 		this.tokyo = null;
 	}
-
+	
+	enableButton(id){
+		document.getElementById(id).disabled = false;
+		document.getElementById(id).style.background = "#00cc00";	
+	}
+	
+	disableButton(id){
+		document.getElementById(id).disabled = true;
+		document.getElementById(id).style.background = "grey";		
+	}
+	
 	maxPoint(){
 		var i;
 		var maxPoint = this.playersArray[0];
 		
-		for(i=0; i<4; i++){
+		for(i=0; i<this.playersArray.length; i++){
 			if(this.playersArray[i].points > maxPoint.points){
 				maxPoint = this.playersArray[i];
 			}			
@@ -150,22 +179,7 @@ class gameRound{
 		return maxPoint;
 	}	
 	
-	deathPlayer(){
-		var i;
-		for(i=0; i<4; i++){	
-			if(this.playersArray[i].life<=9){
-				document.getElementById(this.playersArray[i].id).style.filter = "grayscale(100%)";
-				document.getElementById(this.playersArray[i].id+"Life").style.background = "black";
-				document.getElementById(this.playersArray[i].id+"Points").style.background = "black";
-				document.getElementById(this.playersArray[i].id+"Money").style.background = "black";			
-				document.getElementById(this.playersArray[i].id+"Life").style.color = "red";
-				document.getElementById(this.playersArray[i].id+"Points").style.color = "grey";
-				document.getElementById(this.playersArray[i].id+"Money").style.color = "grey";
-				
-				/*FALTA TERMINAR A MORTE DO JOGADOR*/
-			}
-		}
-	}
+
 	
 	createDices(){
 		this.dices = new Array();
@@ -188,13 +202,9 @@ class gameRound{
 	}	
 	
  	dicesActivate(){
-		document.getElementById("buttonRoll").disabled = false;
-		document.getElementById("buttonRoll").style.background = "#00cc00";
-		document.getElementById("buttonGet").disabled = false;		
-		document.getElementById("buttonGet").style.background = "#00cc00";			
-		
-		document.getElementById("buttonEnd").disabled = true;		
-		document.getElementById("buttonEnd").style.background = "gray";			
+		this.enableButton("buttonRoll");
+		this.enableButton("buttonGet");
+		this.disableButton("buttonEnd");			
 	}
  	
 	dicesPlay(){
@@ -205,77 +215,116 @@ class gameRound{
 		
 		else if(this.countRound==2){
 			this.rollDices();
-			document.getElementById("buttonRoll").disabled = true;
-			document.getElementById("buttonRoll").style.background = "gray";							
-		}	
-
-	}
-	
-	playerBorder(player1, player2){
-		document.getElementById(player1.id).style.border = "none";
-		document.getElementById(player2.id).style.border = "5px solid red";	
-	}
-	
-	doTokyo(){
-		if(document.getElementById("buttonTokyo").innerHTML=="Go to Tokyo?"){
-			this.tokyo = this.player;
-			document.getElementById("tokyoMonster").innerHTML = this.player.name;
-			document.getElementById("tokyoMonsterImg").src = "images/" + this.player.id + ".jpg";
-			document.getElementById("tokyoMonsterImg").alt = this.player.name;	
-		}	
-		
-		if(document.getElementById("buttonTokyo").innerHTML=="Beat the Enemies?"){
-			var i;
-			for(i=0;i<4;i++){
-				if(this.playersArray[i]!=this.player){					
-					this.playersArray[i].life-=results.hand;
-					document.getElementById(this.playersArray[i].id+"Life").innerHTML = "♥ " + this.playersArray[i].life;
-				}
-			}
+			this.disableButton("buttonRoll");
 		}
-		
-		if(document.getElementById("buttonTokyo").innerHTML=="Beat the King?"){
-			this.tokyo.life-=results.hand;
-			document.getElementById(this.tokyo.id+"Life").innerHTML = "♥ " + this.tokyo.life;
-			
-			if(confirm(this.tokyo.name + " want to get out of Tokyo?")){
-				this.getOutTokyo();
-			}					
-		}			
-		
-		document.getElementById("buttonTokyo").disabled = true;
-		document.getElementById("buttonTokyo").style.background = "gray";	
-		
-	}	
+	}
 	
-	getOutTokyo(){
-		/*REDUNDANTE COM DOTOKYO*/
-		this.tokyo.tokyo = 0; 		
+/***********************************************************************
+ * 
+ * DEATH OF THE PLAYER FUNCTION
+ * 
+***********************************************************************/		
+	cleanTokyo(){
+		this.tokyo = null;
+		document.getElementById("tokyoMonster").innerHTML = "";
+		/*DESCOBRIR COMO TIRAR A IMAGEM"*/
+		document.getElementById("tokyoMonsterImg").src = "";
+		document.getElementById("tokyoMonsterImg").alt = "";	
+	}
+	
+	deathPlayer(thePlayer){
+		thePlayer.colorsOfDeath();					
+		this.playersArray.splice(this.playersArray.indexOf(thePlayer),1);		
+	}	
+
+/***********************************************************************
+ * 
+ * TOKYO FUNCTIONS 
+ * 
+***********************************************************************/	
+	enterTokyo(){
 		this.tokyo = this.player;
 		document.getElementById("tokyoMonster").innerHTML = this.player.name;
 		document.getElementById("tokyoMonsterImg").src = "images/" + this.player.id + ".jpg";
 		document.getElementById("tokyoMonsterImg").alt = this.player.name;	
 	}
 	
+	beatEnemies(){
+		var i;
+			
+		for(i=0; i<=this.playersArray.length; i++){			
+			if(this.playersArray[i]!=this.player){
+				console.log("enter here");
+				console.log(results.hand);									
+				this.playersArray[i].life-=results.hand;
+				document.getElementById(this.playersArray[i].id+"Life").innerHTML = "♥ " + this.playersArray[i].life;	
+				
+				console.log(this.playersArray[i].life);					
+			}
+		}
+		/*
+		for(i=0; i<=this.playersArray.length; i++){
+			console.log("enter here");
+			if(this.playersArray[i].isDead()){
+				this.deathPlayer(this.playersArray[i]);
+			}
+		}*/
+	}	
+	
+	
+	beatKing(){
+		this.tokyo.life-=results.hand;
+		document.getElementById(this.tokyo.id+"Life").innerHTML = "♥ " + this.tokyo.life;
+		
+		if(confirm(this.tokyo.name + " want to get out of Tokyo?")){
+			if(this.tokyo.isDead(this.tokyo)){
+				this.deathPlayer(this.tokyo);							
+			}
+			this.getOutTokyo();
+		}
+	}
+	
+	doTokyo(){
+		if(document.getElementById("buttonTokyo").innerHTML=="Go to Tokyo?"){
+			this.enterTokyo();
+		}	
+		
+		if(document.getElementById("buttonTokyo").innerHTML=="Beat the Enemies?"){
+			this.beatEnemies();
+		}
+		
+		if(document.getElementById("buttonTokyo").innerHTML=="Beat the King?"){
+			this.beatKing();
+		}			
+
+		this.disableButton("buttonTokyo");
+	}	
+	
+	getOutTokyo(){
+		this.tokyo.tokyo = 0; 		
+		this.enterTokyo();
+	}
+
+
+/***********************************************************************
+ * 
+ * RESULTS FUNCTIONS
+ * 
+ **********************************************************************/	
 	handResults(){	
+		this.enableButton("buttonTokyo");		
 		/*if Tokyo is empty, the player can choose go to Tokyo*/
 		if(this.tokyo==null){
-			document.getElementById("buttonTokyo").disabled = false;		
-			document.getElementById("buttonTokyo").style.background = "#00cc00";
 			document.getElementById("buttonTokyo").innerHTML = "Go to Tokyo?";	
 		}		
 			
 		/*If the player is in Tokyo*/
 		else if(this.tokyo==this.player){
-			document.getElementById("buttonTokyo").disabled = false;		
-			document.getElementById("buttonTokyo").style.background = "#00cc00";
 			document.getElementById("buttonTokyo").innerHTML = "Beat the Enemies?";					
 		}
 		
 		/*If Tokyo is ocupied by another player*/
 		else if(this.tokyo!=this.player){
-			document.getElementById("buttonTokyo").disabled = false;		
-			document.getElementById("buttonTokyo").style.background = "#00cc00";
 			document.getElementById("buttonTokyo").innerHTML = "Beat the King?";					
 		}
 	}	
@@ -294,15 +343,9 @@ class gameRound{
 		}
 	}
 	
-	gainResults(){				
-			
-		/*REDUNDANTE*/
-		document.getElementById("buttonRoll").disabled = true;
-		document.getElementById("buttonRoll").style.background = "gray";
-		
-		document.getElementById("buttonEnd").disabled = false;		
-		document.getElementById("buttonEnd").style.background = "#00cc00";
-		
+	gainResults(){			
+		this.disableButton("buttonRoll");
+		this.enableButton("buttonEnd");	
 						
 		/*Get the dices's results*/		
 		results.getResults(this.dices);
@@ -328,35 +371,49 @@ class gameRound{
 		document.getElementById(this.player.id+"Points").innerHTML = "★ " + this.player.points;
 		document.getElementById(this.player.id+"Money").innerHTML = "⚡ " + this.player.money;
 
-		document.getElementById("buttonGet").disabled = true;
-		document.getElementById("buttonGet").style.background = "gray";		
-		
-				
+		this.disableButton("buttonGet");				
 		}
+
+
+	winGame(){
+		if(this.maxPoint().points>=20 || this.playersArray.length<=1){			
+			this.disableButton("buttonRoll");
+			this.disableButton("buttonGet");
+			this.disableButton("buttonEnd");						
+			this.disableButton("buttonTokyo");		
+			alert("Congratulations! " + this.maxPoint().name + " won the game!")
+		}
+	}
 		
+
+
+/***********************************************************************
+ * THE END TURN FUNCTION
+ **********************************************************************/		
 	endTurn(){		
 		/*Change player border*/
-		this.playerBorder(this.playersArray[playerIndex], this.playersArray[(playerIndex+1)%4]);
-		
+		document.getElementById(this.player.id).style.border = "none";
+				
 		/*Change player index to get the next player*/
-		playerIndex = (playerIndex+1)%4;
-						
+		playerIndex = (this.playersArray.indexOf(this.player)+1)%this.playersArray.length;
+								
 		/*Get the next player*/
-		this.player = round.playersArray[playerIndex];
-		
+		this.player = round.playersArray[playerIndex];		
+		document.getElementById(this.player.id).style.border = "5px solid red";
+				
 		/*Clear the results*/
 		results.clearResults();
 		
 		/*Tokyo Button*/
 		document.getElementById("buttonTokyo").innerHTML="Tokyo";
-		document.getElementById("buttonTokyo").disabled = true;
-		document.getElementById("buttonTokyo").style.background = "grey";
+		
+		this.disableButton("buttonTokyo");
 
 		/*Incremente the Player's Tokyo Index*/
 		if(this.tokyo != null){
 			this.tokyo.tokyo+=1;
 			
-			if(this.tokyo.tokyo%4==0){
+			if(this.tokyo.tokyo%this.playersArray.length==0){
 			this.tokyo.points+=2;
 			/*REDUNDANTE: PRINT POINTS*/
 			document.getElementById(this.player.id+"Points").innerHTML = "★ " + this.player.points;
@@ -369,21 +426,9 @@ class gameRound{
 		/*Create new dices*/
 		this.createDices();		
 		
-		/*The death of a player*/
-		this.deathPlayer();	
-				
 		/*The end of the game*/
-		if(this.maxPoint().points>=20){
-			document.getElementById("buttonRoll").disabled = true;
-			document.getElementById("buttonRoll").style.background = "gray";
-			document.getElementById("buttonGet").disabled = true;		
-			document.getElementById("buttonGet").style.background = "gray";					
-			document.getElementById("buttonEnd").disabled = true;		
-			document.getElementById("buttonEnd").style.background = "gray";	
-			document.getElementById("buttonTokyo").disabled = true;
-			document.getElementById("buttonTokyo").style.background = "gray";		
-			alert("Congratulations! " + this.maxPoint().name + " won the game!")
-		}			
+		this.winGame();
+					
 	}	
 	
 }
@@ -397,10 +442,10 @@ var playerIndex = 0;
 round.player = round.playersArray[playerIndex];		
 
 function gamePlay(){	
-		round.playerBorder(round.playersArray[3], round.playersArray[0]);
-
-		document.getElementById("buttonStart").disabled = true;
-		document.getElementById("buttonStart").style.background = "gray";		
+		document.getElementById(round.player.id).style.border = "5px solid red";
+		
+		/*DESATIVAR BOTOES*/
+		round.disableButton("buttonStart");	
 
 		round.dicesActivate();
 		round.createDices();
